99622627257c562c829c5ea2d19626ca6c0a9946
Tushar0913 <165733532+Tushar0913@users.noreply.github.com>
2025-07-18 17:50:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel("gemini-2.0-flash")
